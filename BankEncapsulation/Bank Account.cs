50c60207057053fedf9db2f626c9cebacea92b04
aburbank <ashlynburbank@gmail.com>
2020-09-02 14:01:14 -0500

    finished exercise

---
+++ BankEncapsulation/Bank Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class Bank_Account
+    {
+        public Bank_Account()
+        {
+        }
+
+        private double balance = 0;
+
+        public void Deposit()
+        {
+            Console.WriteLine($"Please type deposit amount.");
+            var depositAmount = double.Parse(Console.ReadLine());
+            balance += depositAmount;
+        }
+
+        public double GetBalance()
+        {
+            Console.WriteLine($"Current balance: {balance}");
+            Console.WriteLine($"Have a great day.");
+            return balance;
+            
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ashlynBurbank = new Bank_Account();
+
+            ashlynBurbank.Deposit();
+
+            ashlynBurbank.GetBalance();
+
+        }
+    }
+}
